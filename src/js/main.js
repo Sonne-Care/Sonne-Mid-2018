e1a3a8b53e12174ded4fd3afa6566d2e2d0dd1ee
Craig Fowler <craig@howler.la>
2018-06-18 18:07:08 -0700

    things

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,25 @@ function formstone(){
   $('.carousel').carousel();
   $('.swap').swap();  
   $(".sticky").sticky();
-  $(".lightbox").lightbox();
+  //$(".lightbox").lightbox();
+  $('.benefit-item').swap({
+    collapse: true
+  });  
+  $(".checkpoint").checkpoint({
+    reverse: true
+  });
+}
+
+function instagram(){
+  // 67437a0d2a934c4cba086cd5837bdce9
+  jQuery.fn.spectragram.accessData = {
+    accessToken: '1119864202.21af4c5.9ee0eee607f84393aa572f4236c64051'
+  };
+  $('.insta-feed-container').spectragram('getUserFeed',{
+    max: 4,
+    //size: "small",
+    wrapEachWith: '<div class="fs-cell fs-lg-auto fs-md-auto fs-sm-half photo">'
+  });
 }
 
 function headerScroll(){
@@ -20,6 +38,7 @@ function headerScroll(){
 function init(){
   formstone();
   headerScroll();
+  instagram();
   const tilt = $('.js-tilt').tilt({
     maxTilt: 3,
     scale: 1.01,
@@ -28,6 +47,12 @@ function init(){
   feather.replace({
     'stroke-width': 1,
   });
+  var value = $.cookie('closesignup');
+  if (value != 'true'){
+    setTimeout(function(){
+      $('.signup-close').click();  
+    },1000);
+  }
 }
 
 init();
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -4,6 +4,7 @@
 // @codekit-prepend "../bower_components/formstone/dist/js/transition.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/analytics.js"
 // @codekit-prepend "../bower_components/formstone/dist/js/mediaquery.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/cookie.js"
 
 // Formstone Components
 // @codekit-append "../bower_components/formstone/dist/js/swap.js"
@@ -11,9 +12,10 @@
 // @codekit-append "../bower_components/formstone/dist/js/background.js"
 // @codekit-append "../bower_components/formstone/dist/js/dropdown.js"
 // @codekit-append "../bower_components/formstone/dist/js/sticky.js"
-// @codekit-append "../bower_components/formstone/dist/js/lightbox.js"
+// @codekit-append "../bower_components/formstone/dist/js/checkpoint.js"
 
 // Other Plugins
 // @codekit-append "../bower_components/infinite-scroll/dist/infinite-scroll.pkgd.js"
 // @codekit-append "../bower_components/feather-icons/dist/feather.js"
+// @codekit-append "../bower_components/jquery-spectragram-master/spectragram.min.js"
 // @codekit-append "../node_modules/tilt.js/dest/tilt.jquery.js"
