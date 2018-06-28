a56ae2e5ae476f8adcf373ecead09d107da60659
Craig Fowler <craig@howler.la>
2018-06-28 14:51:44 -0700

    yolo

--- src/js/main.js
+++ src/js/main.js
@@ -59,4 +59,14 @@ function init(){
   }
 }
 
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null){
+    return null;
+  }
+  else{
+    return decodeURI(results[1]) || 0;
+  }
+}
+
 init();
